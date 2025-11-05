7914915a4f4f9833a950ddbd7225f890ff332bc6
changho100 <chbeak@naver.com>
2025-11-05 23:44:36 +0900

    DB 한글 깨짐 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,8 @@ INSERT INTO item (name) VALUES ('마우스');
 INSERT INTO item (name) VALUES ('Item 3 from H2');
 INSERT INTO item (name) VALUES ('Item 4 from H2');
 INSERT INTO item (name) VALUES ('아이패드');
+INSERT INTO item (name) VALUES ('컴퓨터');
+INSERT INTO item (name) VALUES ('마우스');
+INSERT INTO item (name) VALUES ('Item 3 from H2');
+INSERT INTO item (name) VALUES ('Item 4 from H2');
+INSERT INTO item (name) VALUES ('아이패드');
